84e473b4b247533139f71233d838d12ab31aacf0
hejiabin <jiabin.he@coconutislandstudio.com>
2022-02-23 18:56:08 +0800

    misc

--- Assets/CommonAutoUI/UtilityWidgets/EasyScrollView.cs
+++ Assets/CommonAutoUI/UtilityWidgets/EasyScrollView.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 
-
-[RequireComponent(typeof(ScrollRect))]
 public class EasyScrollView : MonoBehaviour
 {
-    private enum Dir
+    public enum Dir
     {
         vertical,
         horizontal,
@@ -21,41 +20,33 @@ public class EasyScrollView : MonoBehaviour
     [SerializeField] private Vector2 m_itemInterval;
     [SerializeField] private int m_perItemCount = 1;                // 同行/列Item个数
 
-    private ScrollRect m_scrollRect;
-    private RectTransform m_itemContainer;
-
-    private Dir m_direction;
+    [SerializeField]private ScrollRect m_scrollRect;
+    [SerializeField]private RectTransform m_itemContainer;
+    [SerializeField] private Dir m_direction;
 
     private int m_itemCount;
-    private List<GameObject> m_itemGoList = new List<GameObject>();
+    private int m_maxVisualCount;
+
+    private List<GameObject> m_pendingItemList = new List<GameObject>();
+    private float m_currentScrollPosotion;
+    private Dictionary<int, GameObject> m_itemDic = new Dictionary<int, GameObject>();
+    private List<int> m_currentPageIndeies = new List<int>();
 
 
     private void Awake()
     {
-        m_scrollRect = GetComponent<ScrollRect>();
-        m_itemContainer = m_scrollRect.viewport.transform.GetChild(0).GetComponent<RectTransform>();
-
         m_scrollRect.onValueChanged.AddListener(onScrollChange);
-
-        // 根据ScrollRect属性判定是横向还是纵向的
-        m_direction = Dir.vertical; //TODO
-    }
-
-
-    // Start is called before the first frame update
-    private void Start()
-    {
     }
 
     // Update is called once per frame
-    private void Update()
+    private void Update() 
     {
-        //TODO
+        refreshView();
     }
 
     private void onScrollChange(Vector2 arg0)
     {
-        //TODO 
+        refreshView();
     }
 
 
@@ -72,43 +63,50 @@ public class EasyScrollView : MonoBehaviour
         }
         else if(m_direction == Dir.horizontal)
         {
-            //TODO 
+            m_itemContainer.sizeDelta = new Vector2((m_itemSize.x + m_itemInterval.x) * realCount , m_itemContainer.sizeDelta.y);
         }
 
         // precreate the items
-        if (m_itemGoList.Count == 0)
+        if (m_pendingItemList.Count == 0 && m_itemDic.Count == 0)
         {
-            int maxVisualCount = 0;
+            m_maxVisualCount = 0;
             
             if (m_direction == Dir.vertical)
             {
                 float viewHeight = GetComponent<RectTransform>().rect.height;
-
                 float viewCount = viewHeight / (m_itemSize.y + m_itemInterval.y);
 
-                maxVisualCount = Mathf.FloorToInt(viewCount);
+                m_maxVisualCount = Mathf.CeilToInt(viewCount) + 1;
+                m_maxVisualCount *= m_perItemCount;
 
-                if ((viewCount - maxVisualCount) > float.Epsilon) // 多余
-                    maxVisualCount++;
-
-                maxVisualCount *= 2;
-
-                maxVisualCount = Math.Min(maxVisualCount, count);
+                m_maxVisualCount = Math.Min(m_maxVisualCount, count);
             }
-            else
+            else if(m_direction == Dir.horizontal)
             {
                 float viewWidth = GetComponent<RectTransform>().rect.width;
+                float viewCount = viewWidth / (m_itemSize.x + m_itemInterval.x);
 
-                //TODO 
+                m_maxVisualCount = Mathf.CeilToInt(viewCount) + 1;
+                m_maxVisualCount *= m_perItemCount;
+
+                m_maxVisualCount = Math.Min(m_maxVisualCount, count);
             }
 
-            for (int i = 0; i < maxVisualCount; i++)
+            for (int i = 0; i < m_maxVisualCount; i++)
             {
                 GameObject go = Instantiate(m_itemPrefab, m_itemContainer);
                 go.transform.localScale = Vector3.one;
 
-                m_itemGoList.Add(go);
+                recycleItem(go);
             }
+        }
+        else
+        {
+            // 回收之前的Item 
+            foreach (GameObject go in m_itemDic.Values)
+                recycleItem(go);
+
+            m_itemDic.Clear();
         }
 
         if (m_direction == Dir.vertical)
@@ -119,22 +117,52 @@ public class EasyScrollView : MonoBehaviour
 
     private void initVerticalItems()
     {
-        float currentPosition = m_itemContainer.localPosition.y;
+        m_currentScrollPosotion = m_itemContainer.localPosition.y;
 
-        int startIndex = Mathf.FloorToInt( currentPosition / (m_itemSize.y + m_itemInterval.y) ) * m_perItemCount;
+        int startIndex = Mathf.FloorToInt(m_currentScrollPosotion / (m_itemSize.y + m_itemInterval.y) ) * m_perItemCount;
+        startIndex = Mathf.Clamp(startIndex, 0, int.MaxValue);
 
-        for(int i = 0; i < m_itemGoList.Count; i++)
+        m_currentPageIndeies.Clear();
+
+        for(int i = 0; i < m_maxVisualCount; i++)
         {
             int index = startIndex + i;
-            m_itemGoList[i].transform.localPosition = getItemPosition(index);
 
-            ON_ITEM_FILL(index, m_itemGoList[i]);
+            if (index >= m_itemCount)
+                break;
+
+            GameObject itemGo = allocItem();
+            itemGo.transform.localPosition = getItemPosition(index);
+            m_itemDic.Add(index, itemGo);
+            ON_ITEM_FILL(index, itemGo);
+
+            m_currentPageIndeies.Add(index);
         }
     }
 
     private void initHorizontalItems()
     {
-        //TODO 
+        m_currentScrollPosotion = m_itemContainer.localPosition.x;
+
+        int startIndex = Mathf.FloorToInt(m_currentScrollPosotion / (m_itemSize.x + m_itemInterval.x)) * m_perItemCount;
+        startIndex = Mathf.Clamp(startIndex, 0, int.MaxValue);
+
+        m_currentPageIndeies.Clear();
+
+        for (int i = 0; i < m_maxVisualCount; i++)
+        {
+            int index = startIndex + i;
+
+            if (index >= m_itemCount)
+                break;
+
+            GameObject itemGo = allocItem();
+            itemGo.transform.localPosition = getItemPosition(index);
+            m_itemDic.Add(index, itemGo);
+            ON_ITEM_FILL(index, itemGo);
+
+            m_currentPageIndeies.Add(index);
+        }
     }
 
     private Vector2 getItemPosition(int index)
@@ -143,14 +171,107 @@ public class EasyScrollView : MonoBehaviour
 
         if(m_direction == Dir.vertical)
         {
-            //TODO 
+            int row = index / m_perItemCount;
+            int col = index % m_perItemCount;
+
+            float rowOffset = (m_itemContainer.rect.width - (m_itemSize.x * m_perItemCount + m_itemInterval.x * (m_perItemCount - 1))) / 2.0f + m_itemSize.x * 0.5f;
+
+            position.x = rowOffset + col * (m_itemSize.x + m_itemInterval.x);
+            position.y = -(m_itemSize.y * 0.5f + m_itemInterval.y + row * (m_itemSize.y + m_itemInterval.y));
         }
         else if(m_direction == Dir.horizontal)
         {
-            //TODO 
+            int row = index % m_perItemCount;
+            int col = index / m_perItemCount;
+
+            float colOffset = (m_itemContainer.rect.height - (m_itemSize.y * m_perItemCount + m_itemInterval.y * (m_perItemCount - 1))) / 2.0f + m_itemSize.y * 0.5f;
+
+            position.x = m_itemSize.x * 0.5f + m_itemInterval.x + col * (m_itemSize.x + m_itemInterval.x);
+            position.y = -(colOffset + row * (m_itemSize.y + m_itemInterval.y));
         }
 
         return position;
     }
 
+    private void refreshView()
+    {
+        float scrollPosition = 0;
+
+        if (m_direction == Dir.vertical)
+            scrollPosition = m_itemContainer.localPosition.y;
+        else if (m_direction == Dir.horizontal)
+            scrollPosition = m_itemContainer.localPosition.x;
+
+        if (Mathf.Abs(m_currentScrollPosotion - scrollPosition) < float.Epsilon)
+            return;
+
+        if (m_direction == Dir.vertical)
+            refreshVerticalView(scrollPosition);
+        else if (m_direction == Dir.horizontal)
+            refreshHorizontalView(scrollPosition);
+
+        m_currentScrollPosotion = scrollPosition;
+    }
+
+    private void refreshVerticalView(float scrollPosition)
+    {
+        int startIndex = Mathf.FloorToInt(scrollPosition / (m_itemSize.y + m_itemInterval.y)) * m_perItemCount;
+        startIndex = Mathf.Clamp(startIndex, 0, int.MaxValue);
+
+        refreshViewItems(startIndex);
+    }
+
+    private void refreshHorizontalView(float scrollPosition)
+    {
+        int startIndex = Mathf.FloorToInt(-scrollPosition / (m_itemSize.x + m_itemInterval.x)) * m_perItemCount;
+        startIndex = Mathf.Clamp(startIndex, 0, int.MaxValue);
+
+        refreshViewItems(startIndex);
+    }
+
+    private void refreshViewItems(int startIndex)
+    {
+        List<int> pageIndeies = new List<int>();
+
+        for (int i = 0; i < m_maxVisualCount; i++)
+        {
+            int index = startIndex + i;
+
+            if (index >= m_itemCount)
+                break;
+
+            pageIndeies.Add(index);
+        }
+
+        foreach (int oldIndex in m_currentPageIndeies.Except<int>(pageIndeies))
+        {
+            recycleItem(m_itemDic[oldIndex]);
+            m_itemDic.Remove(oldIndex);
+        }
+
+        foreach (int newIndex in pageIndeies.Except<int>(m_currentPageIndeies))
+        {
+            GameObject itemGo = allocItem();
+            itemGo.transform.localPosition = getItemPosition(newIndex);
+            m_itemDic.Add(newIndex, itemGo);
+            ON_ITEM_FILL(newIndex, itemGo);
+        }
+
+        m_currentPageIndeies = pageIndeies;
+    }
+
+    private GameObject allocItem()
+    {
+        GameObject go = m_pendingItemList.First<GameObject>();
+        m_pendingItemList.Remove(go);
+        go.SetActive(true);
+
+        return go;
+    }
+
+    private void recycleItem(GameObject item)
+    {
+        m_pendingItemList.Add(item);
+        item.SetActive(false);
+    }
 }
